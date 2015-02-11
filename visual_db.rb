52eb08597c192fc8b2ba5ca13a6a4d86b3a3c9e9
at1as <jason@willems.ca>
2015-02-10 21:38:41 -0500

    Adding ALTER TABLE and SHOW COLUMNS commands Default DB Connection timeout 60s > 5s Fixed some API path issues

--- visual_db.rb
+++ visual_db.rb
@@ -15,7 +15,8 @@ configure do
 	$db_name    = false			# Name of currently selected Database
 	$db_tables  = false		  # List of Tables for currently selected Database
 	$table_name = false		  # Name of currently selected Table
-	$db_error   = nil       # To propagate error/success from SQL queries
+  $table_cols = nil       # Table Column information
+  $db_error   = nil       # To propagate error/success from SQL queries
 
   set :show_exceptions, true
   set :raise_errors, true
@@ -24,10 +25,13 @@ end
 
 def sql_connect(host, username = "root", password = "", database = "", port = 3306)
 	begin
-		$sql_conn = Mysql.new(host, username, password, database, port)
-	rescue Mysql::Error
+    $sql_conn = Mysql.init
+    $sql_conn.options(Mysql::OPT_CONNECT_TIMEOUT, 5)
+		$sql_conn.real_connect(host, username, password, database, port)
+    redirect '/databases'
+	rescue Mysql::Error => e
 		$sql_conn = false
-		$db_error = "Error connecting to Database => #{Mysql::Error}"
+		error_logger("Error connecting to MySQL at #{host}:#{port}", e)
 	end
 end
 
@@ -38,7 +42,7 @@ end
 
 def connection_redir(location)
   if !$sql_conn
-    $db_error = "Bind to MySql"
+    $db_error = "Connect to a MySQL instance before configuring databases"
     redirect "#{location}"
   end
 end
@@ -50,11 +54,35 @@ def database_redir(location)
   end
 end
 
+def get_table
+  "#{$db_name}.#{$table_name}"
+end
 
+def set_table(db, table)
+  begin
+    $full_table = $sql_conn.query "SELECT * FROM #{db}.#{table}"
+  rescue
+    $full_table = $sql_conn.query "SELECT * FROM #{get_table}"
+  end
+end
+
+def error_logger(message, exception)
+  $db_error = "#{message} => #{exception.errno} : #{exception.error}"
+end
+
+
+## Filters
 before do
+  connection_redir '/' unless ['/', '/connect', '/disconnect'].include? request.path_info
+  $db_error = nil if request.post?
+end
+
+after do
   # TODO: Error notifications persist too long
-  next unless request.post?
-  $db_error = nil
+  #next unless request.post?
+  $table_cols = nil
+  #next unless request.get?
+  #$db_error = nil
 end
 
 
@@ -69,7 +97,7 @@ end
 
 post '/connect' do
 	port = Integer(params[:port]) rescue nil
-	sql_connect(params[:hostname], params[:username], params[:password], nil, port)
+	sql_connect(params[:hostname], params[:username], nil, params[:password], port)
 
 	redirect '/'
 end
@@ -82,11 +110,10 @@ end
 
 ## Databases
 get '/databases' do
-	connection_redir '/'
  
   show_dbs = $sql_conn.query "SHOW DATABASES"
   $db_list = []
-  show_dbs.each do |db| $db_list += db end
+  show_dbs.each { |db| $db_list += db }
   
   erb :databases
 end
@@ -94,7 +121,7 @@ end
 post '/databases' do
 	$db_name = params[:select_database]
 	$db_tables, $table_name, $full_table = false, false, false
-	redirect '/databases'
+	redirect '/tables'
 end
 
 post '/database' do
@@ -119,17 +146,17 @@ end
 
 ## Selected Database - All Tables
 get '/tables' do
-	connection_redir '/'
   database_redir '/databases'  
 		
   db_tables = $sql_conn.query "SHOW TABLES from #{$db_name}"
   $db_tables = []
+  #db_tables.each { |table| $db_tables += table }
   db_tables.each do |table|
     $db_tables += table
   end
 
   if $table_name
-    $full_table = $sql_conn.query "SELECT * FROM #{$db_name}.#{$table_name}"
+    set_table($db_name, $table_name)
   end
 
   erb :tables
@@ -142,7 +169,7 @@ post '/tables' do
 end
 
 
-## Selected Database - Specific Table 
+## Selected Database - Specific Table Actions
 post '/table' do
   if params[:action] == "create"
     begin
@@ -163,57 +190,75 @@ post '/table' do
 end
 
 post '/table/query' do
+
   if params[:query_action] == "insert"
     begin
-      $sql_conn.query "INSERT INTO #{$db_name}.#{$table_name} #{params[:set_params]}"
-      $full_table = $sql_conn.query "SELECT * FROM #{$db_name}.#{$table_name}"
+      $sql_conn.query "INSERT INTO #{get_table} #{params[:set_params]}"
     rescue Mysql::Error => e
-      $db_error = "Error inserting #{params[:set_params]} into #{$db_name} => #{e.errno} : #{e.error}"
-      $full_table = $sql_conn.query "SELECT * FROM #{$db_name}.#{$table_name}"
+      error_logger("Error inserting #{params[:set_params]} into #{$db_name}", e)
+    ensure
+      set_table($db_name, $table_name)
     end
+  
   elsif params[:query_action] == "delete"
     begin
-      $sql_conn.query "DELETE FROM #{$db_name}.#{$table_name} WHERE #{params[:query_params]}"
-      $full_table = $sql_conn.query "SELECT * FROM #{$db_name}.#{$table_name}"
+      $sql_conn.query "DELETE FROM #{get_table} WHERE #{params[:query_params]}"
     rescue Mysql::Error => e
-      $db_error = "Error removing #{params[:query_params]} from #{$db_name} => #{e.errno} : #{e.error}"
-      $full_table = $sql_conn.query "SELECT * FROM #{$db_name}.#{$table_name}"
+      error_logger("Error removing #{params[:query_params]} from #{$db_name}", e)
+    ensure
+      set_table($db_name, $table_name)
     end
+  
   elsif params[:query_action] == "filter"
     begin
-      $full_table = $sql_conn.query "SELECT * FROM #{$db_name}.#{$table_name} WHERE #{params[:query_params]}"
+      $full_table = $sql_conn.query "SELECT * FROM #{get_table} WHERE #{params[:query_params]}"
     rescue Mysql::Error => e
-      $db_error = "Error filtering #{$table_name} by #{params[:query_params]} => #{e.errno} : #{e.error}"
-      $full_table = $sql_conn.query "SELECT * FROM #{$db_name}.#{$table_name}"
+      error_logger("Error filtering #{$table_name} by #{params[:query_params]}", e)
+      set_table($db_name, $table_name)
     end
+  
   elsif params[:query_action] == "update"
     begin
-      $full_table = $sql_conn.query "UPDATE #{$db_name}.#{$table_name} SET #{params[:set_params]} WHERE #{params[:query_params]}"
+      $sql_conn.query "UPDATE #{get_table} SET #{params[:set_params]} WHERE #{params[:query_params]}"
     rescue Mysql::Error => e
-      $db_error = "Error setting #{params[:set_params]} to queries matching #{params[:query_params]} => #{e.errno} : #{e.error}"
-      $full_table = $sql_conn.query "SELECT * FROM #{$db_name}.#{$table_name}"
+      error_logger("Error setting #{params[:set_params]} to queries matching #{params[:query_params]}", e)
+    ensure
+      set_table($db_name, $table_name)
+    end
+  
+  elsif params[:query_action] == "alter"
+    begin
+      $sql_conn.query "ALTER TABLE #{get_table} #{params[:set_params]}" 
+    rescue Mysql::Error => e
+      error_logger("Error setting #{params[:set_params]}", e)
+    ensure
+      set_table($db_name, $table_name)
+    end
+  
+  elsif params[:query_action] == "showcols"
+    begin
+      $table_cols = $sql_conn.query "SHOW COLUMNS FROM #{get_table}"
+    rescue Mysql::Error => e
+      error_logger("Error showing columns", e)
+    ensure
+      set_table($db_name, $table_name)
     end
   end
 
   erb :tables
-  #redirect '/tables'
 end
 
 
 ## Manual Query
 get '/query' do
-  connection_redir '/'
-
   erb :query
 end
 
 post '/query' do
   begin
-    query = params[:query]
-    if query[-1, 1] == ";" then query << ";" end
-    $sql_conn.query "#{query}"
+    $sql_conn.query "#{params[:query]}"
   rescue Mysql::Error => e
-    $db_error = "Query \"#{query}\" resulted in error => #{e.errno} : #{e.error}"
+    error_logger("Query \"#{params[:query]}\" resulted in error", e)
   end
 
   erb :query
@@ -238,5 +283,11 @@ post '/csv' do
 
     send_file csv_file, :filename => csv_file, :type => :csv
   end
+end
+
+
+## Default to Connection page
+not_found do
+  redirect '/'
 end
 
